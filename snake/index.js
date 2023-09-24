4d0cf4d36614d8b7063a364060b36809078c7249
TheJakpot0 <138360814+TheJakpot0@users.noreply.github.com>
2023-09-24 23:51:21 +0600

    Update index.js

--- snake/index.js
+++ snake/index.js
@@ -29,7 +29,7 @@ let directionDelta = {
     [Direction.UP]: {x:0, y: -1},
     [Direction.DOWN]: {x:0, y: 1},
     [Direction.LEFT]: {x:-1, y: 0},
-    [Direction.RIGHT]: {x:1, y:0},
+    [Direction.RIGHT]: {x:1, y:1},
 }
 
 // game state
